be22c7b01fe3bf78bdc9ffa93a4535accf397551
刘春华 <chunhua.liu@91power.com>
2019-12-06 09:52:28 +0800

    code

---
+++ day4/component_app/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const staticPath = path.join(__dirname, 'public');
+const app = express();
+
+const staticMiddleware = express.static(staticPath);
+
+app.use(staticMiddleware);
+
+
+const port = 5000;
+app.listen(port, function(err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('http://localhost:' + port);
+});
