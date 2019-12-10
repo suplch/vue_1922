167b40753f617859600399c33d4db6457c01c818
刘春华 <chunhua.liu@91power.com>
2019-12-10 10:13:59 +0800

    code

--- day7/maoapp/vue.config.js
+++ day7/maoapp/vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     devServer: {
       proxy: {
-        '/user': {
+        '/user': {  // 当浏览器发起 以 /user 打头的请求时, 将请求转发给 localhost:5000 服务
             target: 'http://localhost:5000'
         },
-        '/product': {
+        '/product': { // 当浏览器发起 以 /product 打头的请求时, 将请求转发给 localhost:3000 服务
             target: 'http://localhost:3000'
         }
       }
